ecb698591c9a6f1eb735a5c833fbc139f46f5c81
Felipe Daniel <felipe.dandev@gmail.com>
2024-02-17 00:48:04 -0300

    :art: mute message to pt-br

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Result of sum is invalid: Result %d. Wait Value : %d", total, 30)
+		t.Errorf("Resultado de Soma foi inválido: resultado %d. Valor esperado : %d", total, 30)
 	}
 }
